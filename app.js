9df6c254f0a2aade0b5598570974f17071d5415a
Ahmet Tunç Aras <74108753+Latth@users.noreply.github.com>
2022-01-04 23:15:04 +0300

    About size change and navbar added sticky + backgr

--- app.js
+++ app.js
@@ -9,3 +9,18 @@ function menuButtonActive() {
 menuButtons.forEach((item) =>{
     item.addEventListener('click', menuButtonActive)
 })
+
+
+
+function animations() {
+    var controller = new ScrollMagic.Controller();
+
+
+    const t1 = gsap.timeline({defaults: {ease: "power1.InOut"}})
+    
+    
+    t1.fromTo(nav,1, {y: "-100%"}, {y: "0%"})
+}
+
+
+animations()
